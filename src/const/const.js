05e3d12f528b6f215e96a34ba2242c391a7c57d1
Hung Ngo <nghung179123@gmail.com>
2025-05-25 13:33:54 +0700

    navigate + protected route

---
+++ src/const/const.js
@@ -0,0 +1,4 @@
+export const DOMAIN_ADMIN = 'https://souvenirhub-api.onrender.com/';
+export const LOCAL_STORAGE = {
+  ACCOUNT_ADMIN: 'account_admin',
+};
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,14 +1,24 @@
-import React, { lazy } from 'react'
-
+import { lazy } from 'react'
+// import ProtectedRoute from './protected/protectedRoute'
+import {ROUTER_URL} from '../const/router.const'
 // Lazy load components
 const Home = lazy(() => import('../pages/Home'))
 const About = lazy(() => import('../pages/about'))
 const Contact = lazy(() => import('../pages/contact'))
 
+// Example userRole, replace with actual user role from your auth context or state
+// const userRole = "admin" // or get from context/store
+
 const routes = [
-  { path: '/', element: <Home /> },
-  { path: '/about', element: <About /> },
-  { path: '/contact', element: <Contact /> },
+  { path: ROUTER_URL.COMMON.HOME, element: <Home /> },
+  {
+    path: ROUTER_URL.COMMON.ABOUT,
+    element:<About />
+  },
+  {
+    path: ROUTER_URL.COMMON.CONTACT,
+    element: <Contact />
+  }
 ]
 
 export default routes
