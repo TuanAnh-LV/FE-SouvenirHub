126762b8265322a71e567b96c2d549a0f4d0bf21
maxvercoder <161512068+HungFPTU@users.noreply.github.com>
2025-08-09 15:24:02 +0700

    Update const.js

--- src/const/const.js
+++ src/const/const.js
@@ -1,6 +1,6 @@
 // export const DOMAIN_ADMIN = 'https://souvenirhub-api.onrender.com/api';
 // export const DOMAIN_ADMIN = 'http://localhost:3000/api';
-export const DOMAIN_ADMIN = 'https://api.souvenirhub.io.vn//api';
+export const DOMAIN_ADMIN = 'https:/api.souvenirhub.io.vn//api';
 
 export const LOCAL_STORAGE = {
   ACCOUNT_ADMIN: 'account_admin',
